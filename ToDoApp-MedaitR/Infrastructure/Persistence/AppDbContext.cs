9388c0307d36a87bff6d585f3c6a9303288f84d3
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-21 17:23:44 +0300

    feat: add User model and register it in DbContext

--- ToDoApp-MedaitR/Infrastructure/Persistence/AppDbContext.cs
+++ ToDoApp-MedaitR/Infrastructure/Persistence/AppDbContext.cs
@@ -9,5 +9,6 @@ namespace ToDoApp_MedaitR.Infrastructure.Persistence
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<ToDoItem> ToDoItems { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
