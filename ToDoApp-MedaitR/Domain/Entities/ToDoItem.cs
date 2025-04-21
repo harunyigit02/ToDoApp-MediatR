53796446727f2e030ea4896dfb825ca24e39fa61
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-21 17:09:25 +0300

    feat: add TodoItem model and configure DbContext table

---
+++ ToDoApp-MedaitR/Domain/Entities/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp_MedaitR.Domain.Entities
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ ToDoApp-MedaitR/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ToDoApp_MedaitR.Domain.Entities;
+
+namespace ToDoApp_MedaitR.Infrastructure.Persistence
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
