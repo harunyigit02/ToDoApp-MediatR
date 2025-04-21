a7e49ef8edb77552e5a03e60140f2cbce6fc1d78
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-21 17:46:31 +0300

    feat: Implement query handler to get todos by user

---
+++ ToDoApp-MedaitR/Application/ToDoItems/Handlers/GetToDoItemsByUserHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp_MedaitR.Application.ToDoItems.Queries;
+using ToDoApp_MedaitR.Domain.Entities;
+using ToDoApp_MedaitR.Infrastructure.Persistence;
+
+namespace ToDoApp_MedaitR.Application.ToDoItems.Handlers
+{
+    public class GetToDoItemsByUserHandler : IRequestHandler<GetToDoItemsByUserQuery, List<ToDoItem>>
+    {
+        private readonly AppDbContext _context;
+
+        public GetToDoItemsByUserHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<ToDoItem>> Handle(GetToDoItemsByUserQuery request, CancellationToken cancellationToken)
+        {
+            var todos = await _context.ToDoItems
+                .Where(t => t.UserId == request.Id)
+                .ToListAsync();
+            return todos;
+        }
+    }
+}
